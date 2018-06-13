19e1c62ee29321f5f6b6a387f9be7a438e364544
MohanaPriya1991 <40225549+MohanaPriya1991@users.noreply.github.com>
2018-06-13 10:33:26 +0530

    Add files via upload

---
+++ business_Account_Creation.py
@@ -0,0 +1,107 @@
+# Business Account Creation
+# Initial Coding : S.Mohana Priya
+# Description: Will automate the task of changing the RecordType of
+#              an Account to "Business Account" if status
+#              is "Approved by HomeOffice"
+
+import pandas as pd
+import numpy as np
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from pandas import DataFrame, read_excel, merge, read_csv
+import os
+import openpyxl
+import time
+
+os.chdir('C:\\Users\\m.priya.sankarrao\\Documents\\PYTHON\\Business_Accout_Creation')
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+# Extracting Data from Production
+from simple_salesforce import Salesforce
+import pandas as pd
+
+sf = Salesforce(username='data.admin@celgene.com.full2', password='celgene2',security_token='', sandbox=True)
+
+#query = sf.query_all("SELECT Id, Account_Status_CELG__c,recordtype.name from account where recordtype.name ='New Business Account' and Account_Status_CELG__c = 'Approved by Home Office' ")
+query = sf.query_all("SELECT Id, Account_Status_CELG__c, RecordTypeId,Territory_vod__c from account where RecordTypeId ='012U00000001foq' and Account_Status_CELG__c = 'Approved by Home Office' ")
+
+
+ID = []
+STATUS = []
+RECORD_TYPE = []
+TERRITORY = []
+
+#print(query)
+
+
+for record in query['records']:
+    ID.append(record['Id'])
+    STATUS.append(record['Account_Status_CELG__c'] or '')
+    RECORD_TYPE.append(record['RecordTypeId'] or '')
+    TERRITORY.append(record['Territory_vod__c'] or '')
+    #TERRITORIES.append(record[''] or '')
+
+#print(ID)
+record_dict = {}
+record_dict['ID'] = ID
+record_dict['ACCOUNT_STATUS_CELG__C'] = STATUS
+record_dict['RecordTypeId'] = RECORD_TYPE
+record_dict['Territory_vod__c'] = TERRITORY
+
+df = pd.DataFrame(record_dict)
+df.to_excel('Business_Account_BEFORE_'+timestr+'.xlsx')
+
+
+n = len(ID)
+#print(n)
+
+rec_type = '012U00000001fop' # Id for Business Account
+
+if (n == 0):
+    print ("No Records with Status --APPROVED BY HOME OFFICE")
+else :
+    print("Fetched %s records with status--APPROVED BY HOME OFFICE" % n)
+
+ID_AFTER = []
+REC_TYPE_AFTER = []
+TERRI = []
+
+for i in range(n):
+    accId=ID[i]
+    print ("ID is "+accId)
+    sf.Account.update(accId,{'RECORDTYPEID': rec_type}) #Updating from "New Business Account" to "Business Account" 
+    #time.sleep(10)
+    #query = sf.query_all("SELECT Id, RecordTypeId, Territory_vod__c from account where Id = '%s'" % accId)
+
+
+for i in range(n):
+    accId=ID[i]
+    query = sf.query_all("SELECT Id, RecordTypeId, Territory_vod__c from account where Id = '%s'" % accId)
+
+    for record in query['records']:
+        ID_AFTER.append(record['Id'])
+        REC_TYPE_AFTER.append(record['RecordTypeId'] or '')
+        TERRI.append(record['Territory_vod__c'] or '')
+
+
+record_dict1 = {}
+record_dict1['ID'] = ID
+record_dict1['RecordTypeId'] = REC_TYPE_AFTER
+record_dict1['Territory_vod__c'] = TERRI
+
+df1 = pd.DataFrame(record_dict1)
+df1.to_excel('Business_Account_AFTER_'+timestr+'.xlsx')
+    
+
+#query = sf.query_all("SELECT Id, Account_Status_CELG__c, RecordTypeId,Territory_vod__c from account where Id in %s", % str(ID)
+#query = sf.query_all("SELECT Id, Account_Status_CELG__c, RecordTypeId,Territory_vod__c from account where Id in (' + ','.join(map(str, Id)) + ')")
+                     
+                     
+#query = sf.query_all("SELECT Id, Account_Status_CELG__c,recordtype.name from account where recordtype.name ='New Business Account' and Account_Status_CELG__c = 'Approved by Home Office' ")
+#ID_AFTER = []
+
+#for record in query['records']:
+ #   ID_AFTER.append(record['Id'])
+    #TERRITORIES.append(record[''] or '')
+
+#print(ID_AFTER)'''
---
+++ green_white_Sticker_Creation.py
@@ -0,0 +1,130 @@
+#Green Sticker/White Sticker Creation
+#Initial Coding : S.Mohana Priya
+#Description : Automating the Process of Green/White Sticker Creation
+#              Based on report from Business.
+
+import pandas as pd
+import numpy as np
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from pandas import DataFrame, read_excel, merge, read_csv
+import os
+import openpyxl
+import time
+import win32com.client as win32
+import sys
+
+os.chdir('C:\\Users\\m.priya.sankarrao\\Documents\\PYTHON\\Green_White_Create')
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+# Extracting Data from Production
+from simple_salesforce import Salesforce
+import pandas as pd
+
+sf = Salesforce(username='data.admin@celgene.com.full2', password='celgene2',security_token='', sandbox=True)
+
+def create_green():
+    PROD_NAME = input("Enter the Product Name : ")
+    EXT_ID = input("Enter the External Id : ")
+    TYPE = input("Enter the Product Type : ")
+
+    prod = sf.Product_vod__c.create({'Name':str(PROD_NAME),
+    'External_ID_vod__c':str(EXT_ID),
+    'Product_Type_vod__c':str(TYPE),
+    'Parent_Product_vod__c':'a00U000000RYKUcIAP',                                 
+    'VExternal_Id_vod__c':str(EXT_ID)})
+    print("New Product Created")
+    #invoking  Apex Trigger-SampleLimitSet
+
+
+
+def create_white():
+    PROD_NAME = input("Enter the Product Name : ")
+    EXT_ID = input("Enter the External Id : ")
+    TYPE = input("Enter the Product Type : ")
+
+    prod = sf.Product_vod__c.create({'Name':str(PROD_NAME),
+    'External_ID_vod__c':str(EXT_ID),
+    'Product_Type_vod__c':str(TYPE),
+    'Parent_Product_vod__c':'a00U000000YQeydIAD',
+    'VExternal_Id_vod__c':str(EXT_ID)})
+    print("New Product Created")
+
+
+def create_promotional():
+    PROD_NAME = input("Enter the Product Name : ")
+    EXT_ID = input("Enter the External Id : ")
+    TYPE = input("Enter the Product Type : ")
+
+    prod = sf.Product_vod__c.create({'Name':str(PROD_NAME),
+    'External_ID_vod__c':str(EXT_ID),
+    'Product_Type_vod__c':str(TYPE),
+    'Parent_Product_vod__c':'a00U000000RYKUmIAP',
+    'VExternal_Id_vod__c':str(EXT_ID)})
+    print("New Product Created")
+
+
+def create_user_profile():
+    PROD_NAME = input("Enter the Product NAME : ") 
+    NEW_PROF = input("Enter the USER Profile : ")
+
+    query = sf.query_all("SELECT Id from Product_vod__c where Name = '%s'" % PROD_NAME)
+
+    ID = []
+
+    for record in query ['records']:
+        ID.append(record['Id'])
+       
+
+    id_len = len(ID)
+    for i in range (id_len):
+        PROD_ID = ID[i]#-----> fetching Product Id
+        print("The Product Id is : ",PROD_ID)
+    
+    query = sf.query_all("SELECT OwnerId from My_Setup_Products_vod__c where USER_PROFILE__C = '%s'" % NEW_PROF)
+    OWNER_ID = []
+    #print(query)
+    for record in query ['records']:
+        OWNER_ID.append(record['OwnerId'])
+
+    owner_id_len = len(OWNER_ID)
+    print("No of owner ids is : ",owner_id_len)
+
+    #Removing Duplicat OwnerIds
+    # insert the list to the set
+    list_set = set(OWNER_ID)
+    # convert the set to the list
+    unique_owner = (list(list_set))
+
+    uniq_len = len(unique_owner)
+    print("No Of Unique Owner_ids is : ",uniq_len)
+
+
+    #inserting Product_vod__c to distict ownerids of new user profile
+    for i in range(uniq_len):
+        OWNER=unique_owner[i]
+        pro = sf.My_Setup_Products_vod__c.create({'OwnerId':str(OWNER),
+        'Product_vod__c':str(PROD_ID)})
+
+    print("The Product is assigned with USER_PROFILE : ",NEW_PROF)
+
+    
+
+key = input("Choose Whether GREEN / WHITE / PROMOTIONAL STICKER :\n A = > GREEN\n B => WHITE\n C => PROMOTIONAL\n D => USER PROFILE CREATION\n")
+if (key =='A'):
+    #print("create_green()")
+    create_green() # Function call to Create GREEN STICKER Product.
+    
+elif (key == 'B'):
+    #print("create_white()")
+    create_white() # Function call to Create WHITE STICKER Product.
+
+elif (key =='C'):
+    create_promotional() # Function call to Create PROMOTIONAL STICKER Product.
+
+elif (key == 'D'):
+    create_user_profile()
+
+else :
+    print("Enter Valid Option")
+    exit
